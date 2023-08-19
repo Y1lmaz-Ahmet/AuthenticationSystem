264dd5aa56d5d3b3a5b692789286bccb2bfda3ca
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-08-19 21:55:14 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace User_Registration_And_Login_With_MVC_And_Identity_UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
